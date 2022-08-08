ee5b630d5c8c469e1e939018a7a00bf7ff25fb80
DJONATA DE SOUZA DIAS <djonata.dias@dotgroup.com.br>
2022-08-07 21:10:12 -0300

    Criação do componente content para organizar o código de montagem de movie cards

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+export function Content({ movies }: { movies: MovieProps[] }) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
